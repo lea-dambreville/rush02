56e7fe9ba8fb23f429ba0f63cb4f66f6eaac6d90
Lea Dam <leadambreville@gmai.com>
2026-07-25 18:43:25 +0700

    srcs -in progress

---
+++ srcs/ft_utils.c
@@ -0,0 +1,22 @@
+#include <rush02.h>
+
+int ft_strlen(char *str)
+{
+
+}
+int	ft_strcmp(char *s1, char *s2)
+{
+
+}
+char	*ft_strdup(char *src)
+{
+
+}
+char	*ft_strndup(char *src, int n)
+{
+
+}
+int	ft_atoi(char *str)
+{
+  
+}
