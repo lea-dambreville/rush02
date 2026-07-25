e2c6a2ea413dfebd981489134697e103d11c8363
Lea Dam <leadambreville@gmai.com>
2026-07-25 19:38:11 +0700

    print.c

---
+++ srcs/print.c
@@ -0,0 +1,34 @@
+#include "rush02.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_pustr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '\0')
+	{
+		return ;
+	}
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	print_word(char *word, int *is_first)
+{
+	if (word == NULL)
+	{
+		return ;
+	}
+	if (!(*is_first))
+		ft_putchar(' ');
+	ft_pustr(word);
+	*is_first = 0;
+}
